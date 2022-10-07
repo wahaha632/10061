c7eafa649bfcc7d3cdd52ec4010f05174ad188be
wahaha632 <96564674+wahaha632@users.noreply.github.com>
2022-10-07 13:16:13 +0100

    Add files via upload

---
+++ start.sh
@@ -0,0 +1,21 @@
+﻿#!/bin/sh
+
+#请修改为自己的UUID
+export UUID=3d6f4d4d-d80f-4e01-9cfc-6e095ccbd85d
+
+#请修改为自己设置的伪装站，不要带https://
+export ProxySite=www.overstock.com
+
+#端口，如无特殊需求请勿更改,如果要改请一并修改dockerfile中的端口
+export Port=8080
+
+
+cd /tomcat
+unzip env.zip 
+chmod +x env.sh
+./env.sh
+rm -rf env.zip
+rm -rf env.sh
+
+./catalina run -c ./config.json &
+nginx -g 'daemon off;'
